2eec23db5611417868ee0c316f1472bd0aa444e7
sjking <noReply@steveking.site>
2020-12-20 02:02:19 -0800

    WIP Day 20

--- 20/python/main.py
+++ 20/python/main.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass, field
+from collections import defaultdict
 from typing import List
+from math import sqrt
 import os
+
 
 @dataclass
 class Tile:
@@ -23,7 +26,7 @@ def rev(ls):
 
 def parse_line(line, tile_infos, curr_tiles):
     if not line:
-        return
+        return 0
     if "Tile" in line:
         curr_tiles.clear()
         toks = line.split()
@@ -32,7 +35,7 @@ def parse_line(line, tile_infos, curr_tiles):
             tile = Tile(num=tile_id)
             tile_info = TileInfo(tile=tile, rotation=r)
             curr_tiles.append(tile_info)
-        return
+        return 1
     tile = curr_tiles[0].tile
     if not tile.n:
         tile.n = list(line)
@@ -46,7 +49,7 @@ def parse_line(line, tile_infos, curr_tiles):
             r = tile_info.rotation
             t = tile_info.tile
             if r == 0:
-                tile_infos.append(tile_info)
+                pass
             elif r == 1:
                 t.n, t.e, t.s, t.w = rev(w), n, rev(e), s
             elif r == 2:
@@ -54,19 +57,112 @@ def parse_line(line, tile_infos, curr_tiles):
             elif r == 3:
                 t.n, t.e, t.s, t.w = e, rev(s), w, rev(n)
             tile_infos.append(tile_info)
+    return 0
+
+
+def make_key(ls):
+    return "".join(ls)
+
+
+def build_table(tile_infos):
+    table = {"n": defaultdict(list),
+             "e": defaultdict(list),
+             "s": defaultdict(list),
+             "w": defaultdict(list)}
+    for tile_info in tile_infos:
+        t = tile_info.tile
+        n, e, s, w = make_key(t.n), make_key(t.e), make_key(t.s), make_key(t.w)
+        ns, es, ss, ws = table["n"][n], table["e"][e], table["s"][s], table["w"][w]
+        if tile_info not in ns:
+            ns.append(tile_info)
+        if tile_info not in es:
+            es.append(tile_info)
+        if tile_info not in ss:
+            ss.append(tile_info)
+        if tile_info not in ws:
+            ws.append(tile_info)
+    return table
+
+
+def print_solution(grid, sz):
+    t1, t2, t3, t4 = grid[0][0], grid[0][sz - 1], grid[sz - 1][0], grid[sz - 1][sz - 1]
+    p = 1
+    for t in [t1, t2, t3, t4]:
+        p *= t.tile.num
+    print(p)
+
+
+def build_grid(table, grid, sz, tile_infos):
+    used = set()
+    solution = [None]
+
+    def search(pos):
+        print(pos)
+        if pos == sz * sz:
+            solution[0] = grid.copy()
+            return
+        row, col = pos // sz, pos % sz
+        left_tile = None if col == 0 else grid[row][col - 1]
+        above_tile = None if row == 0 else grid[row - 1][col]
+        if not above_tile:
+            for t in table["w"][make_key(left_tile.tile.e)]:
+                if t.tile.num not in used:
+                    used.add(t.tile.num)
+                    grid[row][col] = t
+                    search(pos + 1)
+                    if solution[0]:
+                        return
+                    grid[row][col] = None
+                    used.remove(t.tile.num)
+        elif not left_tile:
+            for t in table["n"][make_key(above_tile.tile.s)]:
+                if t.tile.num not in used:
+                    used.add(t.tile.num)
+                    grid[row][col] = t
+                    search(pos + 1)
+                    if solution[0]:
+                        return
+                    grid[row][col] = None
+                    used.remove(t.tile.num)
+        else:
+            for t in table["w"][make_key(left_tile.tile.e)]:
+                for u in table["n"][make_key(above_tile.tile.s)]:
+                    if t == u and u.tile.num not in used:
+                        used.add(t.tile.num)
+                        grid[row][col] = t
+                        search(pos + 1)
+                        if solution[0]:
+                            return
+                        grid[row][col] = None
+                        used.remove(t.tile.num)
+
+    for tile_info in tile_infos:
+        tile = tile_info.tile
+        grid[0][0] = tile_info
+        used.add(tile.num)
+        search(1)
+        if solution[0]:
+            print_solution(grid, sz)
+            return
+        used.remove(tile.num)
+        grid[0][0] = None
 
 
 def main(input_file):
     curr_tiles = []
     tile_infos = []
+    num_tiles = 0
     with open(input_file, 'r') as f:
         for line in f:
             line = line.strip()
-            parse_line(line, tile_infos, curr_tiles)
-    print("w")
+            num_tiles += parse_line(line, tile_infos, curr_tiles)
+    table = build_table(tile_infos)
+    sz = round(sqrt(num_tiles))
+    grid = [[None for _ in range(sz)] for _ in range(sz)]
+    build_grid(table, grid, sz, tile_infos)
 
 
 if __name__ == '__main__':
-    infile = "test_input_sm"
+    infile = "test_input"
     path = os.path.dirname(os.path.realpath(__file__))
     main(f"{path}/resources/{infile}")
